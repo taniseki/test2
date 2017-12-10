ddbb7ed06d10671b1bd2dbadd8f26b85b9ba0b8d
tanigawa <tanigawa.8987@gmail.com>
2017-12-10 16:36:05 +0900

    すげー！！

--- src/first/first.java
+++ src/first/first.java
@@ -8,7 +8,7 @@ public class first {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		System.out.println(cal.get(Calendar.MONTH)+1);
-
+		System.out.println("すげー！！");
 	}
 
 }
