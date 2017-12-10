2868ca6fa368f0815556019401c9a9862ac4826e
masakazu seki <masakazu seki@DESKTOP-TDRB5PR>
2017-12-10 16:28:36 +0900

    test

---
+++ src/first/first.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Calendar;
+
+public class first {
+
+
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal.get(Calendar.MONTH)+1);
+
+	}
+
+}
