31b82e4a35210fe74ec50b7573f6811227f9e76b
masakazu seki <masakazu seki@DESKTOP-TDRB5PR>
2017-12-21 22:38:02 +0900

    SelectItemメソッドを分割したい

---
+++ src/first/ItemStore.java
@@ -0,0 +1,19 @@
+package first;
+
+/**
+ * 表示するアイテムを格納するメソッド
+ * @author masakazu seki
+ *
+ */
+public class ItemStore {
+
+			;
+	public static void main(String[] args) {
+
+		int howManyItem = 3;
+		for(int i=0;i<howManyItem;i++){
+
+		}
+	}
+
+}
